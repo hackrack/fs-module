babd58805f7f8b376e8593a2c997733bfdfec5fe
hackrack <mail4umed@yahoo.com>
2017-11-14 16:21:38 -0500

    some changes in watchMe.js

--- watchMe/watchMe.js
+++ watchMe/watchMe.js
@@ -1,16 +1,24 @@
 const fs = require("fs");
-fs.writeFile("watch-me1.txt", "", err => {
-  if (err) {
-    console.log(err);
-  }
-})
+// fs.writeFile("watch-me1.txt", "", err => {
+//   if (err) {
+//     console.log(err);
+//   }
+// })
+//
+// fs.writeFile("watch-me2.txt", "", err => {
+//   if (err) {
+//     console.log(err);
+//   }
+// })
 
-fs.writeFile("watch-me2.txt", "", err => {
-  if (err) {
-    console.log(err);
+fs.watch("watch-me1.txt", (eventType, fileName) => {
+  if (eventType === "change") {
+    fs.readFile("watch-me1.txt", "utf8", (err, data) => {
+      if (err) throw err;
+      fs.writeFile("watch-me2.txt", data, err => {
+        if (err) throw err;
+        console.log("changed");
+      })
+    })
   }
-})
-fs.watch("watch-me1.txt", (curr, prev) => {
-  console.log(curr);
-  console.log(prev);
 })
